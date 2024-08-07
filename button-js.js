c27d4733629a372b6a81637ae3f0c688eb9e7583
Miranda567 <mirandaice62@gmail.com>
2024-08-07 06:26:35 +0100

    Add files via upload

---
+++ button-js.js
@@ -0,0 +1,8 @@
+function redirectTodeposit() {
+  // Redirect to the desired page
+  window.location.href = "deposit.html";
+}
+function redirectTowithdraw() {
+  // Redirect to the desired page
+  window.location.href = "withdraw.html";
+}
